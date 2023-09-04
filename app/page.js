7d0d90ffa9b0b561032b8197d8b0767cf766ff7b
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2023-09-04 23:25:45 +0530

    modified some changes

--- app/page.js
+++ app/page.js
@@ -1,15 +1,5 @@
 import { Footer, Navbar } from "../components";
-import {
-  About,
-  Explore,
-  Feedback,
-  GetStarted,
-  Hero,
-  Insights,
-  WhatsNew,
-  World,
-  Experience,
-} from "../sections";
+import { About, GetStarted, Hero, WhatsNew } from "../sections";
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -18,19 +8,13 @@ const Page = () => (
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
-      <Explore />
     </div>
     <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
-    <Experience />
-    <div className="relative">
-      <Insights />
-      <div className="gradient-04 z-0" />
-      <Feedback />
-    </div>
+
     <Footer />
   </div>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>AniMatedWebApp</title>
+      <title>ThinkIndia</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
