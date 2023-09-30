a6135b10eeb3a930b8c75fcb6d7decd6ac5b06f8
Munquab Anwer <munquabr30@gmail.com>
2023-10-01 00:00:43 +0530

    Added the new contact page basic layout

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,98 +1,118 @@
-import React, { useState } from "react";
-import styles from "../styles";
+import React from "react";
 import { motion } from "framer-motion";
-const Contact = () => {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+import { Controller, useForm } from "react-hook-form";
+import IconEmail from "../app/icons/IconEmail";
+import IconPhone from "../app/icons/IconPhone";
+import IconInfo from "../components/IconInfo";
+import FormElements from "../components/FormElements";
 
-    console.log("fullName", fullName);
-    console.log("email", email);
-    console.log("message", message);
+const Contact = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      message: "",
+      name: "",
+    },
+  });
+
+  const formContainerStyle = {
+    position: "relative",
+    zIndex: 1, // Ensure the form is above the image
   };
 
   return (
-    <>
+    <div className="relative">
+      {/* Image Background */}
       <div
-        className={`${styles.innerWidth}  mx-auto flex flex-col mt-5 justify-center items-center`}
-      >
-        <div id="contactimage">
-          <div className=" shadow-md border-border-slate-400  p-4 mb-0 max-w-xl  m-16 start-e ">
-            <motion.h1
-              initial={{ x: -100 }}
-              animate={{ x: 0 }}
-              transition={{
-                duration: 0.5,
-                delay: 0.2,
-              }}
-              whileHover={{ scale: 0.9, opacity: 1 }}
-              className="text-3xl font-bold  text-center text-slate-800 mt-3"
-            >
-              Contact Us
-            </motion.h1>
-          </div>
-          <motion.div
-            whileHover={{
-              scale: 0.9,
-            }}
-            transition={{
-              layout: { duration: 1, type: "spring" },
-            }}
-            className=" bg-slate-700 p-4 max-w-2xl  rounded-2xl   border  mb-0"
-          >
-            <form
-              onSubmit={handleSubmit}
-              className=" text-white py-4 mt-0 border-t flex flex-col gap-5"
-            >
-              <div className="flex flex-col gap-2">
-                <label htmlFor="fullname">Full Name</label>
-                <input
-                  onChange={(e) => setFullName(e.target.value)}
-                  className="pl-2 shadow-lg  border-slate-400 border rounded-lg h-8 text-black"
-                  type="text"
-                  value={fullName}
-                  name="fname"
-                  id="fullname"
-                  placeholder="Full Name"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="email">Email</label>
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="pl-2 shadow-lg  border-slate-400 border rounded-lg h-8  text-black"
-                  type="text"
-                  name="email"
-                  value={email}
-                  id="email"
-                  placeholder="xyz@gmail.com"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="message">Message</label>
-                <textarea
-                  onChange={(e) => setMessage(e.target.value)}
-                  className=" pl-2 shadow-lg  border-slate-400 border rounded-lg h-40 text-black"
-                  id="message"
-                  name="message"
-                  value={message}
-                  placeholder="Type your message here"
-                ></textarea>
-              </div>
+        className="xl:container mx-auto mb-32 mt-20 w-full h-96"
+        style={{
+          background:
+            "radial-gradient(circle, rgba(255, 255, 255, 0.8) 10%, rgba(255, 255, 255, 0.5) 70%, rgba(0, 0, 0, 0.3) 100%), linear-gradient(to bottom, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%)",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+          backgroundImage: `url(./contactus1.jpg)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+        }}
+      ></div>
 
+      {/* Form Container */}
+      <motion.div
+        className="px-4 sm:w-2/3 lg:w-1/2 mx-auto -mt-72"
+        whileHover={{
+          scale: 0.9,
+        }}
+        transition={{
+          layout: { duration: 1, type: "spring" },
+        }}
+        style={formContainerStyle}
+      >
+        <div className="rounded-lg shadow-lg bg-white  py-10 md:py-12 px-4 md:px-6">
+          <div className="grid grid-cols-2 gap-x-6 mb-12 mx-auto">
+            <IconInfo icon={<IconEmail />} text="contact@us" />
+            <IconInfo icon={<IconPhone />} text="+91 6392285932" />
+          </div>
+          <div>
+            <form>
+              {/* Form fields go here */}
+              <Controller
+                name="name"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <FormElements
+                    type="text"
+                    Label="Name"
+                    placeholder="Enter name here..."
+                    fieldRef={field}
+                    hasError={errors.name?.type === "required"}
+                  />
+                )}
+              />
+              <Controller
+                name="email"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <FormElements
+                    type="email"
+                    Label="Email"
+                    placeholder="Enter email here..."
+                    fieldRef={field}
+                    hasError={errors.email?.type === "required"}
+                  />
+                )}
+              />
+              <Controller
+                name="message"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <FormElements
+                    type="textarea"
+                    Label="Message"
+                    placeholder="Enter message here..."
+                    fieldRef={field}
+                    hasError={errors.message?.type === "required"}
+                  />
+                )}
+              />
               <button
-                className=" border rounded-lg bg-blue-600 text-white h-10 w-56 hover:bg-blue-500"
+                className="w-full px-6 py-3 bg-pink-600 text-white font-medium uppercase rounded shadow-md hover:bg-pink-600 hover:shadow-lg focus:bg-pink-600 focus:outline-none focus:ring-0 active-bg-pink-600"
                 type="submit"
               >
-                Send mesage
+                Send
               </button>
             </form>
-          </motion.div>
+          </div>
         </div>
-      </div>
-    </>
+      </motion.div>
+    </div>
   );
 };
 
