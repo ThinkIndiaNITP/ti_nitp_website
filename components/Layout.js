4bf4cf7d46015dd61f61d0a586e6b7d9131a98fe
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2023-11-04 20:39:02 +0530

    add responsive

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Head from "next/head";
 import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>ThinkIndia</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link
+          rel="stylesheet"
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+        />
+      </Head>
       <div className="pb-12">
         <Navbar />
       </div>
