26f9f9ea9d60c9e37e27367fb4b2a3bb8ab73e2c
Akash Saurav <akash010@aks-MacBook-Air.local>
2024-01-20 01:55:57 +0530

    events page data

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "../styles";
 import { fadeIn } from "../utils/motion";
 
